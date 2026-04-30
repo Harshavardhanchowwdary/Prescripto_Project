d0dff6484392eed9e05c775715f2ba44478d8825
Harshavardhan Channupati <charshavardhan122@gmail.com>
2026-04-30 22:26:33 +0530

    overview Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
 
       <div className='text-center text-2xl pt-10 text-[#707070]'>
         <p>CONTACT <span className='text-gray-700 font-semibold'>US</span></p>
-      </div>
+      </div> 
 
       <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28 text-sm'>
         <img className='w-full md:max-w-[360px]' src={assets.contact_image} alt="" />
